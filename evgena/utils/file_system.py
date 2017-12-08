978bd6613961db97ce36eadd746f9a00e9cea4f2
Stepan Prochazka <prochazka.stepan@gmail.com>
2017-12-08 11:37:19 +0100

    :construction: context manager for reading files or paths added

---
+++ evgena/utils/file_system.py
@@ -0,0 +1,16 @@
+import contextlib
+
+
+@contextlib.contextmanager
+def maybe_open(path_or_fp, *args, **kwargs):
+    if isinstance(path_or_fp, str):
+        f = file_to_close = open(path_or_fp, *args, **kwargs)
+    else:
+        f = path_or_fp
+        file_to_close = None
+
+    try:
+        yield f
+    finally:
+        if file_to_close:
+            file_to_close.close()
