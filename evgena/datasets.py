018bd9ce0152fb6c68228b3a58acf79f1e6e1a6d
Stepan Prochazka <prochazka.stepan@gmail.com>
2018-03-03 16:03:27 +0100

    :construction: large files handling implemented, datasets compacted to recarrays

---
+++ evgena/datasets.py
@@ -0,0 +1,36 @@
+import numpy as np
+
+from typing import Tuple
+from evgena.utils.large_files import maybe_download
+
+
+def load_mnist() -> Tuple[np.recarray, np.recarray, np.ndarray]:
+    train = np.load(maybe_download('datasets/mnist_train.npy')).view(np.recarray)
+    test = np.load(maybe_download('datasets/mnist_test.npy')).view(np.recarray)
+
+    return train, test, np.ndarray([str(d) for d in range(10)])
+
+
+def load_emnist() -> Tuple[np.recarray, np.recarray, np.ndarray]:
+    train = np.load(maybe_download('datasets/emnist_balanced_train.npy')).view(np.recarray)
+    test = np.load(maybe_download('datasets/emnist_balanced_train.npy')).view(np.recarray)
+
+    return (
+        train, test,
+        np.array(
+            [str(d) for d in range(10)] +
+            [chr(c) for c in range(ord('A'), ord('Z') + 1)] +
+            list('abdefghnqrt')
+        )
+    )
+
+# use keras like
+# mapping storage?
+# description storage?
+# splits? - some wrapper around data? data splitted from start?
+
+# X - features - some numpy record (or flat if one type of some shape)
+# y - same as X, optional? - so feature as any other? - part of Example description
+# train as a file (.npy)
+# test as a file (.npy)
+# X, y
--- evgena/utils/large_files.py
+++ evgena/utils/large_files.py
@@ -27,37 +27,21 @@ from .file_system import copyfileobj
 
 lfs = [
     (
-        'datasets/emnist_balanced/train_X',
-        '',
-        NotImplemented
+        'datasets/emnist_balanced_train.npy',
+        '5d6b4ee552c50687b56a92ce443438f612df06263c644596b1fad68451b72d2f',
+        'http://www.ms.mff.cuni.cz/~prochas7/evgena/emnist_balanced_train.npy.gz'
     ), (
-        'datasets/emnist_balanced/train_y',
-        '',
-        NotImplemented
+        'datasets/emnist_balanced_test.npy',
+        'b0eb652b7bcd220b4382cdbde2132347e3ad4f3ada973032f74595fc56850255',
+        'http://www.ms.mff.cuni.cz/~prochas7/evgena/emnist_balanced_test.npy.gz'
     ), (
-        'datasets/emnist_balanced/test_X',
-        '',
-        NotImplemented
+        'datasets/mnist_train.npy',
+        'e76d03149d49d074a0e7da5f5ccb2794ef3c92c518fd2a4238518fc355ad5b0f',
+        'http://www.ms.mff.cuni.cz/~prochas7/evgena/mnist_train.npy.gz'
     ), (
-        'datasets/emnist_balanced/test_y',
-        '',
-        NotImplemented
-    ), (
-        'datasets/mnist/train_X',
-        'ba891046e6505d7aadcbbe25680a0738ad16aec93bde7f9b65e87a2fc25776db',
-        'http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz'
-    ), (
-        'datasets/mnist/train_y',
-        '65a50cbbf4e906d70832878ad85ccda5333a97f0f4c3dd2ef09a8a9eef7101c5',
-        'http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz'
-    ), (
-        'datasets/mnist/test_X',
-        '0fa7898d509279e482958e8ce81c8e77db3f2f8254e26661ceb7762c4d494ce7',
-        'http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz'
-    ), (
-        'datasets/mnist/test_y',
-        'ff7bcfd416de33731a308c3f266cc351222c34898ecbeaf847f06e48f7ec33f2',
-        'http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz'
+        'datasets/mnist_test.npy',
+        '86678d169b399e237d6d70bdda3c2406dcaff88b26f43047f84e1852b06dbe6f',
+        'http://www.ms.mff.cuni.cz/~prochas7/evgena/mnist_test.npy.gz'
     ), (
         'models/best_residual_dropout_nn_emnist_2.h5',
         'cd218391e561e13391cabb290d9d7f9bafb9a1bc843c198c1e8a563179454f15',
@@ -87,14 +71,12 @@ def file_sha256(file_path: str, chunk_size: int = 65536) -> str:
     return checksum.hexdigest()
 
 
-def maybe_download(file_path: str):
+def maybe_download(file_path: str) -> str:
     abs_path = os.path.abspath(file_path)
 
     for rel_path, checksum, url in lfs:
         if abs_path.endswith(rel_path):  # path recognized as large file
-            if os.path.isfile(abs_path) and file_sha256(abs_path) == checksum:  # everything OK
-                return False
-            else:  # needs downloading
+            if (not os.path.isfile(abs_path)) or (file_sha256(abs_path) != checksum):  # needs downloading
                 os.makedirs(os.path.dirname(abs_path), exist_ok=True)
                 with urllib.request.urlopen(url) as response:
                     with gzip.GzipFile(fileobj=response) as in_file:
@@ -104,9 +86,9 @@ def maybe_download(file_path: str):
                 if file_sha256(abs_path) != checksum:
                     raise ValueError('Downloaded large file {!r} checksum mismatch.')
 
-                return True
+            return file_path
 
     raise FileNotFoundError('{!r} is not recognized large file'.format(file_path))
 
 
-# TODO how to get root dir of package??
+# TODO how to get root dir of package??, force usage from root
