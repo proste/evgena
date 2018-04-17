5fc624e2d1fafbb3617a89ab6150ffc8f9bac351
Stepan Prochazka <prochazka.stepan@gmail.com>
2018-04-17 15:58:58 +0200

    :construction: unused code pruned

--- evgena/models_legacy/svm.py
+++
@@ -1,4 +0,0 @@
-from sklearn.svm import SVC
-from sklearn.model_selection import GridSearchCV, ParameterGrid
-
-# TODO functions for finding best model, maybe some to retrieve already trained and saved ones
