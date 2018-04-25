64bc516be1629ab730012ddd327d462fb260f568
Stepan Prochazka <prochazka.stepan@gmail.com>
2018-04-25 22:25:37 +0200

    :construction: images to BHWC added

--- evgena/datasets.py
+++ evgena/datasets.py
@@ -24,6 +24,7 @@ def load_emnist() -> Tuple[np.recarray, np.recarray, np.ndarray]:
         )
     )
 
+
 def load_nprecord(file_name):
     dataset = dict(np.load(maybe_download('datasets/' + file_name)))
     
@@ -33,6 +34,30 @@ def load_nprecord(file_name):
     
     return train, test, synset, dataset
 
+
+def images_to_BHWC(examples: np.ndarray, input_format: str = None) -> np.ndarray:
+    if (input_format is not None) and (len(input_format) != examples.ndim):
+        raise ValueError("input_format has different length from examples.ndim")
+    
+    if input_format == 'HW':
+        return examples.reshape(1, *examples.shape, 1)
+    elif input_format == 'BHW':
+        return examples.reshape(*examples.shape, 1)
+    elif input_format == 'HWC':
+        return exampels.reshape(1, *example.shape)
+    elif input_format is None:
+        if examples.ndim == 2:                  # single gray image
+            return examples.reshape(1, *examples.shape, 1)
+        elif examples.ndim == 3:
+            if examples.shape[2] in [1, 3, 4]:  # hopefully single gray, RGB, RGBA image
+                return examples.reshape(1, *examples.shape)
+            else:                               # multiple gray images
+                return examples.reshape(*examples.shape, 1)
+        elif examples.ndim == 4:                # already 4D BHWC
+            return examples
+        else:
+            raise ValueError("Invalid shape of examples")
+        
 # use keras like
 # mapping storage?
 # description storage?
