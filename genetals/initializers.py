585694b57de4cfdf90f24a713efc9307e02d3bc8
Stepan Prochazka <prochazka.stepan@gmail.com>
2018-04-28 13:35:35 +0200

    :construction: genetals random initializer added

--- genetals/initializers.py
+++ genetals/initializers.py
@@ -1,7 +1,8 @@
+from typing import Sequence, Callable
+
 import numpy as np
 
 from .core import InitializerBase
-from typing import Sequence
 
 
 class RandomBitStringInit(InitializerBase):
@@ -15,12 +16,40 @@ class RandomBitStringInit(InitializerBase):
 
 
 class RandomStdInit(InitializerBase):
-    def __init__(self, individual_shape: Sequence[int], sigma: float = 1, mu: np.ndarray = 0):
+    def __init__(self,
+        individual_shape: Sequence[int],
+        sigma: np.ndarray = 1, mu: np.ndarray = 0
+    ):
         super(RandomStdInit, self).__init__()
-
+        
         self._individual_shape = individual_shape
         self._sigma = sigma
         self._mu = mu
-
+    
     def __call__(self, population_size: int, *args, **kwargs) -> np.ndarray:
-        return self._mu + (self._sigma * np.random.random((population_size,) + tuple(self._individual_shape)))
+        sigma = kwargs.get('sigma', self._sigma)
+        mu = kwargs.get('mu', self._mu)
+        
+        individuals = np.random.standard_normal((population_size,) + tuple(self._individual_shape))
+        
+        return mu + (sigma * individuals)
+
+
+class RandomUniformInit(InitializerBase):
+    def __init(self,
+        individual_shape: Sequence[int],
+        mu: np.ndarray = 0, semi_range: np.ndarray = 1
+    ):
+        super(RandomUniformInit, self).__init__()
+        
+        self._individual_shape = individual_shape
+        self._mu = mu
+        self._semi_range = semi_range
+        
+    def __call__(self, population_size: int, *args, **kwargs) -> np.ndarray:
+        mu = kwargs.get('mu', self._mu)
+        semi_range = kwargs.get('semi_range', self._semi_range)
+        
+        individuals = np.random.random_sample((population_size,) + tuple(self._individual_shape))
+        
+        return self.mu + (self.semi_range * (2 * individuals - 1))
